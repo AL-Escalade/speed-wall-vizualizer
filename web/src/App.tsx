d0fd8697e612a9a8be3623f969d4ac356ed7038b
Guillaume Gautreau <guillaume+github@ghusse.com>
2025-12-09 07:46:33 +0100

    fix: export on mobile

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -29,13 +29,14 @@ function MobileLayout() {
     <div className="h-screen flex flex-col">
       <Header />
       <div className="flex-1 flex flex-col min-h-0 pb-14">
-        {activeTab === 'config' ? (
-          <div className="h-full overflow-y-auto">
-            <Sidebar />
-          </div>
-        ) : (
+        {/* Sidebar visible only on config tab */}
+        <div className={`h-full overflow-y-auto ${activeTab === 'config' ? '' : 'hidden'}`}>
+          <Sidebar />
+        </div>
+        {/* Viewer always rendered for SVG export, hidden on config tab */}
+        <div className={`flex-1 min-h-0 ${activeTab === 'viewer' ? '' : 'hidden'}`}>
           <Viewer />
-        )}
+        </div>
       </div>
       <MobileNav />
     </div>
